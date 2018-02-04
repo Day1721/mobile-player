f190dbf648d3a6b44cafb958e6877ae7901dc821
Day1721 <puzynamarek97@gmail.com>
2018-02-04 16:24:50 +0100

    Initial commit

---
+++ MobilePlayer/MobilePlayer/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace MobilePlayer
+{
+    [Activity(Label = "MobilePlayer", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
